a83d45a380cac8f6d9038095b114c65d7f3a1cd9
Will <willmanfrancia@gmail.com>
2025-07-11 01:09:18 -0400

    Finalice las funciones principales de la app. Agregar, eliminar, limpiar tareas completadas, check para tareas concluídas.

--- TodoList/scripts/script.js
+++ TodoList/scripts/script.js
@@ -1,11 +1,78 @@
+let listaTareas = JSON.parse(localStorage.getItem('listaTareas')) || [];
+const inputElement = document.querySelector('.js-cuadro-texto');
+const listaTareasUl = document.querySelector('.js-lista-tareas');
+
+mostrarListaTareas();
+
 document.querySelector('.js-btn-agregar').addEventListener('click', () => {
+  if (inputElement.value !== '') {
+    const tarea = { texto: inputElement.value, completado: false };
 
-  const inputElement = document.querySelector('.js-cuadro-texto');
-
-  if (inputElement.value != '') {
-    document.querySelector('.js-lista-tareas').innerHTML += `<li>${inputElement.value} <button class='btn js-btn-eliminar'>Eliminar</button></li> `;
+    listaTareas.push(tarea);
+    guardarLista();
+    mostrarListaTareas();
     inputElement.value = '';
-  }
 
+  };
 });
 
+function mostrarListaTareas() {
+
+  document.querySelector('.js-lista-tareas').innerHTML = '';
+
+  listaTareas.forEach((tarea) => {
+
+    const nuevaTarea = document.createElement('li');
+    nuevaTarea.classList.add('js-tarea');
+
+    const checkbox = document.createElement('input');
+    checkbox.type = 'checkbox';
+    checkbox.checked = tarea.completado;
+    checkbox.classList.add('tarea');
+
+    const textoTareaSpan = document.createElement('span');
+    textoTareaSpan.textContent = tarea.texto;
+
+
+    if (tarea.completado) {
+      nuevaTarea.classList.add('tarea-completada');
+    }
+
+    checkbox.addEventListener('change', () => {
+      tarea.completado = !tarea.completado;
+
+
+      guardarLista();
+      mostrarListaTareas();
+    });
+
+    const botonEliminar = document.createElement('button');
+    botonEliminar.textContent = 'Eliminar';
+    botonEliminar.classList.add('btn', 'js-boton-eliminar');
+
+    botonEliminar.addEventListener('click', () => {
+
+      listaTareas = listaTareas.filter(t => t.texto !== tarea.texto);
+      guardarLista();
+      mostrarListaTareas();
+    });
+
+    listaTareasUl.appendChild(nuevaTarea);
+
+    nuevaTarea.appendChild(checkbox);
+    nuevaTarea.appendChild(textoTareaSpan);
+    nuevaTarea.appendChild(botonEliminar);
+
+  });
+
+}
+
+function guardarLista() {
+  localStorage.setItem('listaTareas', JSON.stringify(listaTareas));
+}
+
+document.querySelector('.js-btn-limpiar').addEventListener('click', () => {
+   listaTareas = listaTareas.filter(t => !t.completado);
+   guardarLista();
+   mostrarListaTareas();
+});
