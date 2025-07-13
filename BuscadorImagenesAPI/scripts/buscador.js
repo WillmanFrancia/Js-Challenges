f7c641e0d759af24a99c79695aebee2a2fbb232a
Will <willmanfrancia@gmail.com>
2025-07-12 21:49:44 -0400

    Se creó sistema que se conecta al API de Unsplash y busca imágenes.
    Se le agregó la funcionalidad al botón Buscar, que hace una petición de
    imagenes y que muestra una imagen por cada resultado devuelto de la API

--- BuscadorImagenesAPI/scripts/buscador.js
+++ BuscadorImagenesAPI/scripts/buscador.js
@@ -1,9 +1,53 @@
-const API_KEY = "TU_CLAVE_DE_UNSPLASH_AQUI"; // Reemplaza con tu clave de Unsplash real
+const API_KEY = "API_KEY";
 const inputBusqueda = document.getElementById("textoBusqueda");
 const botonBusqueda = document.getElementById("botonBusqueda");
 const galeriaImagenes = document.getElementById("galeriaImagenes");
 
 botonBusqueda.addEventListener("click", () => {
   const terminoBusqueda = inputBusqueda.value;
-  console.log("Termino de Búsqueda:", terminoBusqueda);
+
+  if (terminoBusqueda) {
+    buscarImagenes(terminoBusqueda);
+  } else {
+    galeriaImagenes.textContent = "Por favor, ingrese un termino de búsqueda.";
+  }
 });
+
+async function buscarImagenes(termino) {
+  const URL_API = `https://api.unsplash.com/search/photos?query=${termino}&client_id=${API_KEY}`;
+
+  try {
+    galeriaImagenes.innerHTML = "<p>Cargando imágenes...</p>";
+
+    const response = await fetch(URL_API);
+
+    if (!response.ok) {
+      throw new Error(
+        `Error de red o API:${response.status} ${response.statusText}`,
+      );
+    }
+
+    galeriaImagenes.innerHTML = "";
+
+    const datos = await response.json();
+
+    if (datos.results.lenght === 0) {
+      galeriaImagenes.innerHTML =
+        "<p>No se encontraron resultados para su búsqueda. Intente con otro término</p>";
+      return;
+    }
+
+    datos.results.forEach((imagen) => {
+      const elementoImagen = document.createElement("img");
+      elementoImagen.src = imagen.urls.small;
+      elementoImagen.alt =
+        imagen.alt_description || imagen.description || "Imágen de Unsplash";
+      elementoImagen.classList.add("webImagen");
+      galeriaImagenes.appendChild(elementoImagen);
+    });
+    console.log(datos);
+  } catch (error) {
+    galeriaImagenes.innerHTML =
+      "<p>No se pudieron cargar las imágenes. Inténtalo de nuevo.</p>";
+  }
+}
