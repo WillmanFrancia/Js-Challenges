eda7bffe60dd6b3af53c74c198828fb3d9460054
Will <willmanfrancia@gmail.com>
2025-07-14 01:34:29 -0400

    Función para cambiar valores está terminado.
    Se agregó funcionalidad a los selectores para que funcionen dependiendo
    del valor. Se utiliza un valor de entrada y se transforma a metros, sin
    importar el valor seleccionado, para luego transformarlo desde metro al
    valor de destino.

--- ConvertidorUnidades/script/convertidor.js
+++ ConvertidorUnidades/script/convertidor.js
@@ -4,9 +4,50 @@ const unidadDestino = document.getElementById("unidadDestino");
 const resultadoTexto = document.getElementById("resultadoTexto");
 
 function realizarConversion() {
-  console.log(Number(valorUsuario.value));
-  console.log(unidadOrigen.value);
-  console.log(unidadDestino.value);
+  let valorEntrada = Number(valorUsuario.value);
+  let valorEnMetros;
+  let resultadoFinal;
+
+  if (isNaN(valorEntrada) || valorEntrada === 0) {
+    resultadoTexto.textContent = "Ingrese un valor válido";
+    return;
+  }
+
+  switch (unidadOrigen.value) {
+    case "metros":
+      valorEnMetros = valorEntrada;
+      break;
+    case "kilometros":
+      valorEnMetros = valorEntrada * 1000;
+      break;
+    case "millas":
+      valorEnMetros = valorEntrada * 1609.34;
+      break;
+    case "centimetros":
+      valorEnMetros = valorEntrada / 100;
+      break;
+    default:
+      break;
+  }
+
+  switch (unidadDestino.value) {
+    case "metros":
+      resultadoFinal = valorEnMetros;
+      break;
+    case "kilometros":
+      resultadoFinal = valorEnMetros / 1000;
+      break;
+    case "millas":
+      resultadoFinal = valorEnMetros / 1609.34;
+      break;
+    case "centimetros":
+      resultadoFinal = valorEnMetros * 100;
+      break;
+    default:
+      break;
+  }
+
+  resultadoTexto.textContent = `${resultadoFinal.toFixed(2)} ${unidadDestino.value}`;
 }
 
 valorUsuario.addEventListener("input", () => {
